e9fd38ea2aa25286d633f9d1f6a3fefc196844e0
Diswarin Saiphanas <136217235+diswarin@users.noreply.github.com>
2024-02-20 17:07:57 +0700

    add logging

--- logs_csv.py
+++ logs_csv.py
@@ -2,6 +2,32 @@ from datetime import datetime
 import time
 import csv 
 import dss
+import logging
+
+def logging_tags():
+    # create logger
+    logger = logging.getLogger('simple_example')
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+    # 'application' code
+    logger.debug('debug message')
+    logger.info('info message')
+    logger.warn('warn message')
+    logger.error('error message')
+    logger.critical('critical message')
 
 logs_now = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 conn = dss.connect()
@@ -36,6 +62,7 @@ def read_tag():
     record_T010_TI12_GR     	= dss.readEqual(conn, data_set, 'GREASE.FAM0101.T010_TI12_GR')
     print (record_T010_TI12_GR)
     write_logs(record_T010_TI12_GR)
+    logging_tags()
 
 def write_logs(logs_data):
     this_moment = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
